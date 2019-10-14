7f97a6b9a34b314e6f2c15f49393b1b9945d119d
madhuragandhe <43580898+madhuragandhe@users.noreply.github.com>
2019-10-14 14:23:59 +0530

    Blue color detection
    Python program to detect the blue color

---
+++ BlueDetection.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+video_capture=cv2.VideoCapture(0)
+while True:
+    ret,frame=video_capture.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    lower=np.array([110,50,50])
+    upper=np.array([130,255,255])
+    mask=cv2.inRange(hsv,lower,upper)
+    blue=cv2.bitwise_and(frame,frame,mask=mask)
+    cv2.imshow("original ",frame)
+    cv2.imshow("blue ",blue)
+    if cv2.waitKey(0):
+        break
+
+cv2.destroyAllWindows()
+video_capture.release()
