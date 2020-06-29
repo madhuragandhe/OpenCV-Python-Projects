98e2575fa3b59f29c5f68a1181ec8cb1316b42d5
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-06-29 13:22:10 +0530

    Update BlueDetection.py

--- BlueDetection.py
+++ BlueDetection.py
@@ -3,7 +3,7 @@ import numpy as np
 
 video_capture=cv2.VideoCapture(0)
 while True:
-    ret,frame=video_capture.read()
+    frame=video_capture.read()
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     lower=np.array([110,50,50])
     upper=np.array([130,255,255])
